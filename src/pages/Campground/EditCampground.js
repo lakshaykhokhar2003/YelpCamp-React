a70580b6841ff74218caead2566b83471666129e
Lakshay <lakshaykhokhar2003@gmail.com>
2023-12-01 17:07:04 +0530

    feat:added protection to routes when not logged in

--- src/pages/Campground/EditCampground.js
+++ src/pages/Campground/EditCampground.js
@@ -1,6 +1,16 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 const EditCampground = () => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/login');
+        }
+    }, [isAuthenticated, navigate]);
     const params = useParams()
     return (<div>
         <h1>Edit Campground for {params.campgroundId}</h1>
--- src/pages/Campground/NewCampground.js
+++ src/pages/Campground/NewCampground.js
@@ -1,7 +1,20 @@
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 const NewCampground = () => {
-    return <div>
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/login');
+        }
+    }, [isAuthenticated, navigate]);
+
+    return (<div>
         <h1>New Campground</h1>
-    </div>
-}
+    </div>);
+};
 
 export default NewCampground;
