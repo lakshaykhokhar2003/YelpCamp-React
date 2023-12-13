b978e1e790548c1283d501b3ca5b00df8c8d4706
Lakshay <lakshaykhokhar2003@gmail.com>
2023-12-13 18:43:05 +0530

    feat: added more security using helmet and mongo-sanitize

--- src/backend.js
+++ src/backend.js
@@ -6,21 +6,27 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const session = require('express-session')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
 const {MongoClient, ServerApiVersion} = require('mongodb');
+
 const passport = require("passport");
 const LocalStrategy = require('passport-local')
 const uri = "mongodb://localhost:27017/yelp-camp";
 
 const User = require('./models/userModel')
-const campgroundRoutes = require('./routes/campgrounds')
-const reviewRoutes = require('./routes/reviews')
-const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgroundRoutes')
+const reviewRoutes = require('./routes/reviewsRoutes')
+const userRoutes = require('./routes/usersRoutes')
 
 
 const secret = process.env.REACT_APP_SECRET || 'thisshouldbeabettersecret!';
 
+app.use(mongoSanitize({replaceWith: '_'}))
+app.use(helmet())
 app.use(express.json());
 app.use(session({secret, resave: false, saveUninitialized: true}))
+
 app.use(cors({
     origin: 'http://localhost:3001',
     credentials: true,
@@ -28,18 +34,35 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+const scriptSrcUrls = ['https://stackpath.bootstrapcdn.com/', 'https://api.tiles.mapbox.com/', 'https://api.mapbox.com/', 'https://kit.fontawesome.com/', 'https://cdnjs.cloudflare.com/', 'https://cdn.jsdelivr.net/'];
+const styleSrcUrls = ['https://kit-free.fontawesome.com/', 'https://stackpath.bootstrapcdn.com/', 'https://api.mapbox.com/', 'https://api.tiles.mapbox.com/', 'https://fonts.googleapis.com/', 'https://use.fontawesome.com/', 'https://cdn.jsdelivr.net/'];
+const connectSrcUrls = ['https://api.mapbox.com/', 'https://a.tiles.mapbox.com/', 'https://b.tiles.mapbox.com/', 'https://events.mapbox.com/'];
+const fontSrcUrls = [];
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self"],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", ...styleSrcUrls],
+        workerSrc: ["'self'", 'blob:'],
+        objectSrc: ["'none"],
+        imgSrc: ["'self'", 'blob:', 'data:', 'https://res.cloudinary.com/', 'https://images.unsplash.com/', 'https://source.unsplash.com/'],
+        fontSrc: ["'self'", ...fontSrcUrls]
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('Error connecting to MongoDB:', err);
 });
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -56,6 +79,7 @@ async function run() {
         await client.close();
     }
 }
+
 run().catch(console.dir);
 
 app.use('/campgrounds', campgroundRoutes)
